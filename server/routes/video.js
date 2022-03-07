1771e4833d1fd05aefc247a4e673aa12548bcbd1
sak-17 <seakwon17@gmail.com>
2022-03-07 17:50:28 +0900

    first commit

---
+++ server/routes/video.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const router = express.Router();
+// const { Video } = require("../models/Video");
+
+const { auth } = require("../middleware/auth");
+const multer = require('multer');
+var ffmpeg = require("fluent-ffmpeg");
+const { Video } = require('../models/Video');
+
+// STORAGE MULTER CONFIG
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'video/mp4') {
+        cb(null, true);
+    } else {
+        cb({msg: 'only mp4 is allowed.'}, false);
+    }
+}
+
+const upload = multer({ storage: storage, fileFilter: fileFilter }).single("file");
+
+//=================================
+//             Video
+//=================================
+
+router.post('/uploadfiles', (req, res) => {
+
+    // 비디오를 서버에 저장한다.
+    upload(req, res, err => {
+        if (err) {
+            return res.json({ success: false, err })
+        } else {
+            return res.json({ success: true, filePath: res.req.file.path, fileName: res.req.file.filename })
+        }
+    })
+    
+})
+
+
+router.post('/getVideoDetail', (req, res) => {
+
+    Video.findOne({ "_id" : req.body.videoId })
+        .populate('writer')
+        .exec((err, videoDetail) => {
+            if (err) return res.status(400).send(err)
+            return res.status(200).json({ success: true, videoDetail })
+        })  
+})
+
+
+router.post('/uploadVideo', (req, res) => {
+
+    // 비디오 정보들을 저장한다.
+    const video = new Video(req.body)
+
+    video.save((err, doc) => {
+        if (err) return res.json({ success: false, err })
+        res.status(200).json({ success: true })
+    })
+    
+})
+
+
+router.get('/getVideos', (req, res) => {
+
+    // 비디오를 DB에서 가져와 클라이언트에 보낸다.
+    Video.find()
+        .populate('writer')
+        .exec((err, videos) => {
+            if (err) return res.status(400).send(err);
+            res.status(200).json({ success: true, videos })
+        })
+})
+
+
+router.post('/thumbnail', (req, res) => {
+
+    // 썸네일 생성하고 비디오 러닝타임도 가져오기
+    let thumbsFilePath = ""
+    let fileDuration = ""
+    
+    // 비디오 정보 가져오기
+    ffmpeg.setFfmpegPath("C:\\Program Files\\ffmpeg\\bin\\ffmpeg.exe");
+    ffmpeg.ffprobe(req.body.url, function (err, metadata) {
+        console.dir(metadata);  // all metadata
+        console.log(metadata.format.duration);
+        fileDuration = metadata.format.duration
+    });
+
+    // 썸네일 생성
+    ffmpeg(req.body.url)
+    .on('filenames', function (filenames) {
+        console.log('Will generate ' + filenames.join(', '))
+        console.log(filenames)
+
+        thumbsFilePath = "uploads/thumbnails/" + filenames[0]
+    })
+    .on('end', function () {
+        console.log('Screenshots taken');
+        return res.json({ success: true, thumbsFilePath: thumbsFilePath, fileDuration: fileDuration })
+    })
+    .on('error', function (err) {
+        console.error(err);
+        return res.json({ success: false, err });
+    })
+    .screenshots({
+        // Will take screenshots at 20%, 40%, 60% and 80% of the video
+        count: 3,
+        folder: 'uploads/thumbnails',
+        size: '320x240',
+        // '%b': input basename (filename w/o extension)
+        filename: 'thumbnail-%b.png'
+    })
+
+})
+
+module.exports = router;
